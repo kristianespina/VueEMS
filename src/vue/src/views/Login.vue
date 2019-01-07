56177c9d4e641a4f290bee53af439e413823bf25
Kristian Espina <onelespina@gmail.com>
2019-01-07 22:53:43 +0800

    Added Vue

---
+++ src/vue/src/views/Login.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="login-page">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12 col-md"></div>
+        <div class="col-sm-12 col-md">
+
+          <LoginBox />
+
+        </div>
+        <div class="col-sm-12 col-md"></div>
+      </div>
+    </div>
+
+
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import LoginBox from "@/components/LoginBox.vue"; // @ is an alias to /src
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+@Component({
+  components: {
+    LoginBox
+  }
+})
+export default class Home extends Vue {}
+</script>
